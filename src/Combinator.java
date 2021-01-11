4b3887044d7de0d3ca2ecdd868666e40776e16f8
Jonathan Sphar <jonathansphar@gmail.com>
2021-01-10 18:27:29 -0600

    Switched to iterative-coin approach

--- src/Combinator.java
+++ src/Combinator.java
@@ -2,29 +2,63 @@ import java.util.Arrays;
 
 class Combinator {
 	// Set of denominations (coins)
-	DenomSet coins;
+	private DenomSet coins;
 
-	//Memoization array where [n,m]=# of nth coins to sum to m 
-	int[][] sums;
+	//Memoization array where [n,m]=# of ways to create m from the (0...n)th coins 
+	private int[][] combos;
 
 	Combinator(DenomSet coins){
 		this.coins = coins;
-		sums = new int[coins.getNumCoins()][coins.getTarget()+1];
+
+		combos = new int[coins.getNumCoins()+1][coins.getTarget()+1];
 	}
 
-	public void findSums(){
+	public void findCombos(){
+		// Set base case - 0 can always be made using 0 coins (1 solution)
+		for (int i = 0; i < combos.length; i++) {
+			combos[i][0] = 1;
+		}
+
 		// Loop through every coin
-		for (int i = 0; i < coins.getNumCoins(); i++) {
-			int value = coins.values.get(i);
-			//Start filling in values
-			for(int j=0; j <= (coins.getTarget()/value); j++){
-				sums[i][j*value] = j;
+		for (int i = 1; i <= coins.getNumCoins(); i++) {
+			int value = coins.values.get(i-1);
+			
+			//Try to create each j using this + previous coin combos
+			for(int j=1; j <= coins.getTarget(); j++){
+				if(value <= j){
+					// Add in # smaller combos using these coins
+					combos[i][j] = combos[i-1][j] + combos[i][j-value];
+				}else{
+					// Coin is bigger than j - no new combos 
+					combos[i][j] = combos[i-1][j];
+				}
 			}
 		}
+		System.out.println(Arrays.deepToString(combos));
 	}
 
-	// Traverses the DP array to print out combinations
-	public void findCombos(){
-
+	public void printCombos(){
+		findCombos();
+		printHeader();
+		printCombosHelper(coins.getTarget(), coins.getNumCoins());
 	}
+	
+	private void printCombosHelper(int target, int coinInd){
+		
+	}
+
+	private void printHeader(){
+		for (String name : coins.names) {
+			System.out.printf("%5s",name);
+		}
+		System.out.println();
+	}
+
+	private void printOneCombo(int[] coinsUsed){
+		for (int numCoin : coinsUsed) {
+			System.out.printf("%5d",numCoin);
+		}
+		System.out.println();
+	}
+	
 }
--- src/Currency.java
+++ src/Currency.java
@@ -1,18 +1,21 @@
 public class Currency {
 	public static void main(String[] args) {
 		
-		DenomSet american = new DenomSet("Quarter,4,Dime,10,Nickel,20,Penny,100");
+		DenomSet fullamerican = new DenomSet("Dollar,1,Half,2,Quarter,4,Dime,10,Nickel,20,Penny,100");
 		DenomSet anarchy = new DenomSet("Coin,1.5,Arrowhead,3,Button,150");
+		DenomSet american =  new DenomSet("Quarter,4,Dime,10,Nickel,20,Penny,100");
 		DenomSet mine = new DenomSet("A,2,B,5,C,10");
+		DenomSet theirs = new DenomSet("C,10,B,5,A,2");
 
-		Combinator combinator = new Combinator(mine);
+
+		Combinator combinator = new Combinator(american);
 		
 		System.out.println(mine.toString());
 		System.out.println(mine.getNumCoins());
 		System.out.println(mine.getTarget());
 		System.out.println(mine.values);
 
-		combinator.findSums();
+		combinator.findCombos();
 
 	}
 }
